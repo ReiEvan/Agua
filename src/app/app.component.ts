a0934503429bffb3de881fb119e1890f362bc874
Evan <evasconcelos381@gmail.com>
2025-06-11 12:18:52 -0300

    Aula11/06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {VaporComponent} from './vapor/vapor.component'
+import {ListaDeDisciplinasComponent} from "./lista-de-disciplinas/lista-de-disciplinas.component"
+import { CommonModule } from '@angular/common';
+import { Disciplina } from './disciplina.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,VaporComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,ListaDeDisciplinasComponent,CommonModule],
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'agua';
+
+  selecionado : null | Disciplina = null
+
+  disciplinas = [
+    
+      new Disciplina ('Língua Portuguesa',"Portugues apenas"),
+    
+      new Disciplina ('Arte', "Artes plasticas"),
+
+      new Disciplina ('Educação Física', "Exercicios para o corpo"),
+
+      new Disciplina ('Matemática', "Calculos e suas finalidades"),
+
+      new Disciplina ('Geografia', "Demografia, planicies e afins"),
+
+      new Disciplina ('Ciências', "Natureza e Tecnologias"),
+
+      new Disciplina ('Redação', "Instruções de textos e paragrafos"),
+
+      new Disciplina ('Língua Estrangeira Moderna - Inglês',"Aprendizado de linguas de fora do pais"),
+
+      new Disciplina ('Ensino Religioso', "O ser, caratér e moral"),
+    
+  ];
+
+  selecionar(disciplina: Disciplina) {
+    this.selecionado = disciplina;
+  }
 }
---
+++ src/app/lista-de-disciplinas/lista-de-disciplinas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-disciplinas',
+  templateUrl: './lista-de-disciplinas.component.html',
+  styleUrl: './lista-de-disciplinas.component.css'
+})
+export class ListaDeDisciplinasComponent {
+
+}
